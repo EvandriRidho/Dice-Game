a315d07cc85f499705320ba5210820f13d289e98
unknown <evandriridho321@gmail.com>
2024-09-20 22:12:47 +0700

    Update

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-randomNumber1 = Math.floor(Math.random() * 6) + 1;
-randomNumber2 = Math.floor(Math.random() * 6) + 1;
+function callToAction() {
+    // Membuat Random Number
+    randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-const randomPictures1 = `./images/dice${randomNumber1}.png`
-const randomPictures2 = `./images/dice${randomNumber2}.png`
+    // Membuat untuk images menjadi random
+    const randomPictures1 = `./images/dice${randomNumber1}.png`
+    const randomPictures2 = `./images/dice${randomNumber2}.png`
 
-const img1 = document.querySelector(".img1")
-img1.setAttribute("src", randomPictures1)
+    // Pengambilan DOM dan Menganti src
+    const img1 = document.querySelector(".img1")
+    img1.setAttribute("src", randomPictures1)
+    const img2 = document.querySelector(".img2")
+    img2.setAttribute("src", randomPictures2)
 
-const img2 = document.querySelector(".img2")
-img2.setAttribute("src", randomPictures2)
+    // Pengambilan Dom titleChange
+    const titleChange = document.querySelector("h1")
 
-const titleChange = document.querySelector("h1")
-if (randomNumber1 > randomNumber2) {
-    titleChange.innerHTML = "Player 1 Win the Game"
-} else if (randomNumber1 == randomNumber2) {
-    titleChange.innerHTML = "Draw"
-} else {
-    titleChange.innerHTML = "Player 2 Win the Game"
+    // Validasi Random Number
+    if (randomNumber1 > randomNumber2) {
+        titleChange.innerHTML = "Player 1 Win the Game"
+    } else if (randomNumber1 == randomNumber2) {
+        titleChange.innerHTML = "Draw"
+    } else {
+        titleChange.innerHTML = "Player 2 Win the Game"
+    }
 }
